426ce647d971be9cbafbd395641cf6fd1e79b784
Saeed Asadi <saeedasadi69@gmail.com>
2020-11-05 19:45:04 +0330

    first edit

--- src/routes.js
+++ src/routes.js
@@ -27,6 +27,7 @@ import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
+import Form1 from "views/Form1/Form1.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
@@ -51,6 +52,14 @@ const dashboardRoutes = [
     rtlName: "ملف تعريفي للمستخدم",
     icon: Person,
     component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/Form1",
+    name: "User Form1",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: Person,
+    component: Form1,
     layout: "/admin"
   },
   {
